d06e55b339c1cbed1669cbeadc696da9a98da3bb
BernhardHartmann <bernhardhartmann0012@gmail.com>
2019-11-20 23:56:58 +0100

    Game logic object oriented with classes - "Client" "ClientClass" and "Move"
    missing - akka integration and play with multiple players over n classes.

--- Alcatraz_game_logic/Game_logic/ClassLibrary3/ALcatrazGame.cs
+++ Alcatraz_game_logic/Game_logic/ClassLibrary3/ALcatrazGame.cs
@@ -7,62 +7,50 @@ using Alcatraz;
 namespace Alcatraz
 {
 
-    public class Test : MoveListener
+    public class Test
     {
-        private Alcatraz[] other = new Alcatraz[4];
-        private int numPlayer = 3;
-
+        
         public Test()
         {
-        }
 
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        }
+        
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Test t1 = new Test();
-            Alcatraz a1 = new Alcatraz();
+
             int players = 3;
-          
-            List<Test> testList = new List<Test>();
+
+            ClientClass clientClass = new ClientClass(players);
+            List<Client> clientList = new List<Client>();
+
+            for(int i = 0; i < players; i++)
+            {
+                clientList.Add(clientClass.initializeClient(i,players));
+            }
+
+            List<ClientClass> clientClassList = new List<ClientClass>();
             List<Alcatraz> alcatrazList = new List<Alcatraz>();
 
-            for (int i = 0; i < players; i++)
+            foreach (Client clientItem in clientList)
             {
-
-                t1 = new Test();
-                a1 = new Alcatraz();
-   
-                t1.setNumPlayer(players);
-                //i is in this case the playersID
-                a1.init(players, i);
-                Console.WriteLine("a1.init("+players+" , "+i);
-
-                for (int j = 1; j < players + 1; j++)
-                {
-                    int help = j - 1;
-                    a1.getPlayer(help).Name = "Player " + j;
-
-                    Console.WriteLine("getPlayer("+help+").Name = Player "+j);
-                }
-                
-                alcatrazList.Add(a1);
-                testList.Add(t1);
+                clientClassList.Add(clientItem.getClientClass());
+                alcatrazList.Add(clientItem.getAlcatraz());
+         
             }
+
             int counterForEachTest = 0;
-            foreach (Test testListItem in testList)
+            foreach (ClientClass clientClassItem in clientClassList)
             {
                 int counterForEachAlcatraz = 0;
                 int iterator = 0;
-                foreach(Alcatraz alcatrazItem in alcatrazList)
+                foreach (Alcatraz alcatrazItem in alcatrazList)
                 {
                     if (counterForEachTest != counterForEachAlcatraz)
                     {
-                        testListItem.setOther(iterator, alcatrazItem);
+                        clientClassItem.setOther(iterator, alcatrazItem);
                         Console.WriteLine("t" + counterForEachTest + ".setOther( " + iterator + "," + counterForEachAlcatraz);
                         iterator++;
                     }
@@ -73,7 +61,7 @@ namespace Alcatraz
             for (int ii = 0; ii < players; ii++)
             {
                 alcatrazList[ii].showWindow();
-                alcatrazList[ii].addMoveListener(testList[ii]);
+                alcatrazList[ii].addMoveListener(clientClassList[ii]);
                 alcatrazList[ii].getWindow().FormClosed += new FormClosedEventHandler(Test_FormClosed);
                 alcatrazList[ii].start();
             }
@@ -84,47 +72,6 @@ namespace Alcatraz
         {
             Environment.Exit(0);
         }
-
-        public void setOther(int i, Alcatraz t)
-        {
-            this.other[i] = t;
-        }
-
-        public int getNumPlayer()
-        {
-            return numPlayer;
-        }
-
-        public void setNumPlayer(int numPlayer)
-        {
-            this.numPlayer = numPlayer;
-        }
-
-        public void doMove(Player player, Prisoner prisoner, int rowOrCol, int row, int col)
-        {
-            Console.WriteLine("moving " + prisoner + " to " + (rowOrCol == Alcatraz.ROW ? "row" : "col") + " " + (rowOrCol == Alcatraz.ROW ? row : col));
-            for (int i = 0; i < getNumPlayer()-1; i++)
-            {
-                other[i].doMove(other[i].getPlayer(player.Id), other[i].getPrisoner(prisoner.Id), rowOrCol, row, col);
-                Console.WriteLine("Player " + other[i].getPlayer(player.Id) + "Prisoner " + prisoner + "row " + row + "col" + col);
-            }
-        }
-
-        public void undoMove()
-        {
-            Console.WriteLine("Undoing move");
-        }
-            
-        public void gameWon(Player player)
-        {
-            Console.WriteLine("Player " + player.Id + " wins.");
-        }
-
-        public static void main(String[] args)
-        {
-            Console.WriteLine("main main main");
-
-
-        }
+ 
     }
 }
---
+++ Alcatraz_game_logic/Game_logic/ClassLibrary3/Client.cs
@@ -0,0 +1,45 @@
+﻿using Alcatraz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alcatraz
+{
+    class Client
+    {
+        private ClientClass clientClass;
+        private Alcatraz alcatraz;
+        private int playerID;
+
+        public Client()
+        {
+            
+        }
+        public Client(ClientClass clientClass,Alcatraz alcatraz, int playerID)
+        {
+            this.clientClass = clientClass;
+            this.alcatraz = alcatraz;
+            this.playerID = playerID;
+        }
+
+        public void setPlayerID(int playerID)
+        {
+            this.playerID = playerID;
+        }
+        public int getPlayerID()
+        {
+            return playerID;
+        }        
+
+        public ClientClass getClientClass()
+        {
+            return clientClass;
+        }
+        public Alcatraz getAlcatraz()
+        {
+            return alcatraz;
+        }
+    }
+}
