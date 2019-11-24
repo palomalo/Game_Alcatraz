820659a8ec57f3358a8f25d313efcac59fa00085
BernhardHartmann <bernhardhartmann0012@gmail.com>
2019-11-24 03:33:17 +0100

    final push

---
+++ Alcatraz_game_logic/final_client_logic_akka/final_client_logic_akka/ClientClass.cs
@@ -0,0 +1,92 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Alcatraz
+{
+    class ClientClass
+    {
+
+        private IActorRef localChatActor;
+        private IActorRef child;
+        private ActorSystem actorSystem;
+        Alcatraz clientAlcatraz = new Alcatraz();
+        private string actorSystemName;
+        private static ActorSelection[] remoteChatActorClient;
+        private int playerNumber;
+        private int iterator;       
+
+        public ClientClass(ClientData[] data, string uniqueName)
+        {
+            actorSystemName = uniqueName;
+            actorSystem = ActorSystem.Create(actorSystemName);
+            remoteChatActorClient = new ActorSelection[data.Length];
+
+            child = actorSystem.ActorOf(Props.Create<GameActor>(), uniqueName);
+
+            iterator = 0;
+            //remoteChatActorClient[0] = actorSystem.ActorSelection("s");
+            for (int i = 0; i < data.Length; i++)
+            {
+                actorSystem = ActorSystem.Create(uniqueName);
+                string remoteActorAddressClient1 = data[i].getAddress() + data[i].getPort() + data[i].getUrlAddition();
+                remoteChatActorClient[iterator] = actorSystem.ActorSelection(remoteActorAddressClient1);
+                iterator++;
+            }
+
+            this.localChatActor = actorSystem.ActorOf(Props.Create<GameActor>(), "GameActor");
+            
+        }
+
+        public Client initializeClient(int playerID, int numberOfPlayer, ClientData[] data, string uniqueName)
+        {
+            //Game logic
+            clientAlcatraz.init(numberOfPlayer, playerID);
+
+            for (int j = 1; j < numberOfPlayer + 1; j++)
+            {
+                int help = j - 1;
+                clientAlcatraz.getPlayer(help).Name = "Player " + j;
+            }
+            Client client = new Client(clientAlcatraz, playerID);
+
+            for (int jj = 0; jj < numberOfPlayer-1; jj++)
+            {
+
+                Console.WriteLine("Tell Client" + jj);
+                //System.Threading.Thread.Sleep(5000);
+                Console.WriteLine("send to: " + remoteChatActorClient[jj].Path);
+                Console.WriteLine("send to: " + remoteChatActorClient[jj].PathString);
+
+                remoteChatActorClient[jj].Tell(client, child);
+
+            }
+            playerNumber = numberOfPlayer;
+            return client;
+        }
+
+        public IActorRef getChild()
+        {
+            return this.child;
+        }
+
+        public static ActorSelection[] getRemoteChatActorClient()
+        {
+            return remoteChatActorClient;
+        }    
+
+        public void undoMove()
+        {
+            Console.WriteLine("Undoing move");
+        }
+
+        public void gameWon(Player player)
+        {
+            Console.WriteLine("Player " + player.Id + " wins.");
+        }
+    }
+}
---
+++ Alcatraz_game_logic/final_client_logic_akka/final_client_logic_akka/ClientData.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alcatraz
+{
+    class ClientData
+    {
+        private string address;
+        private int playerID;
+        private string uniqueName;
+        private int port;
+        private string urlAddition;
+
+        public ClientData(string address, int port,string urlAddition,int playerID, string uniqueName)
+        {
+            this.address = address;
+            this.port = port;
+            this.uniqueName = uniqueName;      
+            this.urlAddition = urlAddition;
+            this.playerID = playerID;
+        }
+
+        public void setAddress(string address)
+        {
+            this.address = address;
+        }
+        public string getAddress()
+        {
+            return this.address;
+        }
+        public void setPlayerID(int playerID)
+        {
+            this.playerID = playerID;
+        }
+        public string getPlayerID()
+        {
+            return this.address;
+        }
+        public void setUniqueName(string uniqueName)
+        {
+            this.uniqueName = uniqueName;
+        }
+        public string getUniqueName()
+        {
+            return this.uniqueName;
+        }
+        public void setPort(int port)
+        {
+            this.port = port;
+        }
+        public int getPort()
+        {
+            return this.port;
+        }
+        public void setUrlAddition(string urlAddition)
+        {
+            this.urlAddition = urlAddition;
+        }
+        public string getUrlAddition()
+        {
+            return urlAddition;
+        }
+
+    }
+}
